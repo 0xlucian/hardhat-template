8eeacb47946670db9d41b72c72b2f351e37ccd28
NatSpector.Ai <natspector.ai@gmail.com>
2025-01-15 15:53:07 +0200

    Add NatSpec documentation to contracts/SwapContract.sol

--- contracts/SwapContract.sol
+++ contracts/SwapContract.sol
@@ -1,3 +1,4 @@
+```solidity
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.7.6;
 pragma abicoder v2;
@@ -6,12 +7,25 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 
+/// @title A contract for swapping tokens on Uniswap V3
+/// @notice This contract uses Uniswap V3 to swap a token for another token
 contract SwapContract {
+    /// @notice The Uniswap V3 SwapRouter
     ISwapRouter public immutable swapRouter;
+
+    /// @notice The Uniswap V3 Factory
     IUniswapV3Factory public immutable poolFactory;
+
+    /// @notice The address of the wrapped ETH token
     address public immutable wETHAddress;
+
+    /// @notice The fee tier to be used in swaps
     uint24 public constant feeTier = 3000;
 
+    /// @notice Initializes the contract with Uniswap V3 router, factory, and WETH address
+    /// @param _swapRouter The address of the Uniswap V3 SwapRouter
+    /// @param poolFactory_ The address of the Uniswap V3 Factory
+    /// @param wETHAddress_ The address of the wrapped ETH token
     constructor(
         ISwapRouter _swapRouter,
         IUniswapV3Factory poolFactory_,
@@ -22,9 +36,12 @@ contract SwapContract {
         poolFactory = poolFactory_;
     }
 
-    //******************
-    //*     EVENTS     *
-    //******************
+    /// @notice Emitted after a successful token swap
+    /// @param user The address of the user who executed the swap
+    /// @param tokenIn The address of the input token
+    /// @param tokenOut The address of the output token
+    /// @param amountIn The amount of the input token
+    /// @param amountOut The amount of the output token received
     event SwapExecuted(
         address indexed user,
         address tokenIn,
@@ -33,17 +50,14 @@ contract SwapContract {
         uint256 amountOut
     );
 
-    /// @notice A function that swaps one token for other using UniswapV3
-    /// @dev caller should allow amountIn of tokenIn to this contract before calling this function
-    /// @param amountIn Amount to be spent for the swap
-    /// @param tokenIn Address of the token to be exchanged
-    /// @param tokenOut Address of the token to be received
-    /// @param minAmountOut Minimum amount of the received token
-    /// @param priceLimit This value can be used to set the limit for the price the swap will push the pool to.
-    /// @dev Setting minAmountOut = 0 is a significant risk in production.
-    ///        For a real deployment, this value should be calculated using our SDK or an onchain price oracle -
-    ///        this helps protect against getting an unusually bad price for a trade
-    ///        due to a front running sandwich or another type of price manipulation.
+    /// @notice Swaps an exact amount of one token for as much as possible of another token
+    /// @dev The caller must have approved this contract to spend at least `amountIn` of `tokenIn` before calling
+    /// @param amountIn The amount of the input token to swap
+    /// @param tokenIn The address of the input token
+    /// @param tokenOut The address of the output token
+    /// @param minAmountOut The minimum amount of the output token that must be received for the transaction not to revert
+    /// @param priceLimit The limit for the price the swap will push the pool to, encoded as a sqrtPriceLimitX96 value
+    /// @return amountOut The amount of the output token received from the swap
     function swapTokenForToken(
         uint256 amountIn,
         address tokenIn,
@@ -80,3 +94,4 @@ contract SwapContract {
         amountOut = swapRouter.exactInputSingle(params);
     }
 }
+```
